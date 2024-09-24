5a8fe6cef5edd81ba1fe3272568d998675c5e087
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2024-09-24 13:35:53 +0530

    asx

--- src/Components/Placement_Info.tsx
+++ src/Components/Placement_Info.tsx
@@ -12,6 +12,8 @@ interface PlacementData {
   }
 
 const Placement_Info:React.FC = () => {
+
+    const pageLen = 10;
     const [data, setData] = useState<PlacementData[]>([
             // { rollNo: "20951A6609", name: "V D P GANESH", companyName: "TCS", package: 9.0 },
             // { rollNo: "20951A0463", name: "SURROLA HEMANTH", companyName: "TCS", package: 9.0 },
@@ -40,6 +42,12 @@ const Placement_Info:React.FC = () => {
             // { rollNo: "20951A6648", name: "G SRIKAR", companyName: "TCS", package: 3.36 }
         ]);
 
+    const [showingData,setShowing] = useState<PlacementData[]>([]);
+    const [ShowingIdx,setShowingIdx] = useState<number[]>([0,pageLen]);
+
+    const [data_accending,setAccending] = useState<PlacementData[]>([]);
+    const [data_Decending,setDecending] = useState<PlacementData[]>([]);
+
     const [companyName,setCompanyName] = useState<string>(localStorage.getItem("companyName")||"");
 
     const [Batch,setBatch] = useState<string>("2024");
@@ -64,35 +72,44 @@ const Placement_Info:React.FC = () => {
         })
         .then((res) => {
             const placements: PlacementData[] = res.data.data;
-            console.log(placements);
+            // console.log(placements);
             setLoad(false);
             setData(placements);
             setFetchStat(true);
+            if(placements.length>20){ 
+                setShowingIdx([0,pageLen]);
+            }
+            else{
+                setShowingIdx([0,placements.length]);
+            }
         })
         .catch((err) => {
             console.log(err);
             setSubmmit(false);
         });
     };
-    const Sorted_callFetch = () => {
-        setData([]);
-        setLoad(true);
-        axios.post(url + "/get-placement-info-ordered", {
-            companyName,
-            Batch
-        })
-        .then((res) => {
-            const placements: PlacementData[] = res.data.data;
-            console.log(placements);
-            setLoad(false);
-            setData(placements);
-            setFetchStat(true);
-        })
-        .catch((err) => {
-            console.log(err);
-            setSubmmit(false);
-        });
+
+    const sortByPackageAccending = () => {
+        const sortedData = [...data].sort((a, b) => a.package - b.package);
+        setAccending(sortedData);
+      };
+
+    const sortByPackageDecending = () => {
+        const sortedData = [...data].sort((a, b) => b.package - a.package);
+        setDecending(sortedData);
     };
+
+
+    const [order,setOrder] = useState<number>(0);
+
+    useEffect(()=>{
+        if(order===1){
+            setData(data_accending);
+        }
+        if(order===2){
+            setData(data_Decending);
+        }
+    },[order])
 
     
     useEffect(()=>{
@@ -112,10 +129,52 @@ const Placement_Info:React.FC = () => {
             .catch((Err)=>{
                 console.log(Err);
             })
-
-
         setSubmmit(false);
     },[companyName,Batch]);
+
+
+    useEffect(()=>{
+        sortByPackageAccending();
+        sortByPackageDecending();
+        // console.log(data.length)
+        if(data.length<ShowingIdx[1]){
+            setShowing(data.slice(ShowingIdx[0], data.length));
+        }
+        else {
+            setShowing(data.slice(ShowingIdx[0], ShowingIdx[1]));
+          }
+    },[data,ShowingIdx]);
+
+    const NextPage = () => {
+        const x = ShowingIdx[0];
+        const y = ShowingIdx[1];
+        // console.log(data.length > y)
+        if(data.length > y){
+            if(data.length>y+pageLen){
+                setShowingIdx([y,y+pageLen]);
+            }
+            else{
+                setShowingIdx([y,data.length]);
+            }
+        } 
+    }
+    const PreviousPage = () => {
+        const x = ShowingIdx[0];
+        const y = ShowingIdx[1];
+        // console.log(x)
+        if(x>1){
+            if(x-pageLen>0){
+                setShowingIdx([x-pageLen,y-pageLen]);
+            }
+            if(y===data.length){
+                const c = data.length%pageLen;
+                setShowingIdx([x-pageLen,y-c]);
+            }
+            else{
+                setShowingIdx([0,pageLen]);
+            }
+        }
+    }
 
   return (
     <div className=' bg-black/0  w-full h-full z-40 flex flex-col gap-7 pt-16 items-center pb-24' >
@@ -202,9 +261,40 @@ const Placement_Info:React.FC = () => {
        }
 
         {data.length>0 &&
-            <div className='w-full h-fit bg-slate-600/0  flex justify-center mb-12 px-4'>
-                <div className='w-full overflow-x-auto flex items-center justify-center'>
-                    <Table headings={["SNO", "Roll number", "Name", "Company", "Package"]} rows={data} />
+            <div className='w-full h-fit bg-slate-600/0  flex justify-center  px-4 flex-col '>
+                <div className=' h-10 w-full bg-black/0 flex ic justify-center'>
+                    <div className=' h-full w-[70%] bg-black/0 flex justify-end items-end max-sm:w-full '>
+                        <div className=' px-4 h-fit w-fit py-2 hover:cursor-pointer max-sm:scale-75
+                         flex items-center justify-center gap-3 text-white rounded-lg select-none bg-black/70'>
+                            <div onClick={()=>{
+                                PreviousPage();
+                            }} className=' rotate-0 hover:scale-110 transition-all active:scale-90'>
+                                <svg xmlns="http://www.w3.org/2000/svg" 
+                                        fill="white" height="30px" width="30px" version="1.1" id="Layer_1" viewBox="0 0 330 330">
+                                        <path id="XMLID_6_" 
+                                        d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M205.606,234.394  c5.858,5.857,5.858,15.355,0,21.213C202.678,258.535,198.839,260,195,260s-7.678-1.464-10.606-4.394l-80-79.998  c-2.813-2.813-4.394-6.628-4.394-10.606c0-3.978,1.58-7.794,4.394-10.607l80-80.002c5.857-5.858,15.355-5.858,21.213,0  c5.858,5.857,5.858,15.355,0,21.213l-69.393,69.396L205.606,234.394z"/>
+                                    </svg>
+                                </div>
+                            <p>{`${ShowingIdx[0]+1} to ${ShowingIdx[1]}`}</p>
+                            <div onClick={()=>{
+                                NextPage();
+                            }} className=' rotate-180 hover:scale-110 transition-all active:scale-90'>
+                                <svg xmlns="http://www.w3.org/2000/svg" 
+                                    fill="white" height="30px" width="30px" version="1.1" id="Layer_1" viewBox="0 0 330 330">
+                                    <path id="XMLID_6_" 
+                                    d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M205.606,234.394  c5.858,5.857,5.858,15.355,0,21.213C202.678,258.535,198.839,260,195,260s-7.678-1.464-10.606-4.394l-80-79.998  c-2.813-2.813-4.394-6.628-4.394-10.606c0-3.978,1.58-7.794,4.394-10.607l80-80.002c5.857-5.858,15.355-5.858,21.213,0  c5.858,5.857,5.858,15.355,0,21.213l-69.393,69.396L205.606,234.394z"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-full overflow-x-auto flex items-center justify-center max-sm:pl-16 z-30'>
+                    <Table 
+                    startSno={ShowingIdx[0]}
+                    order={order} 
+                    setOrder={setOrder} 
+                    headings={["SNO", "Roll number", "Name", "Company", "Package"]} 
+                    rows={showingData} />
                 </div>
             </div>
         }
--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -5,29 +5,63 @@ interface RowData {
     rollNo: string;
     name: string;
     companyName: string;
-    package: number;  // Renamed from `package` to `packageAmount`
+    package: number;  
 }
 
 interface TableProps {
     headings: string[];
     rows: RowData[];
+    startSno : number;
+    order:number;
+    setOrder : React.Dispatch<React.SetStateAction<number>>;
 }
 
-const Table: React.FC<TableProps> = ({ headings, rows }) => {
+
+const Table: React.FC<TableProps> = ({ headings, rows ,order, setOrder,startSno}) => {
     return (
         <div >
             <table className="styled-table  max-sm:text-xs " border={4}>
                 <thead className=' border-black border-1'>
                     <tr>
                         {headings.map((head, id) => (
+                            head==="Package" ?
+                            <th className=' hover:cursor-pointer'  onClick={()=>{
+                                if(order===0){
+                                    setOrder(1);
+                                }
+                                if(order===1){
+                                    setOrder(2);
+                                }
+                                if(order===2){
+                                    setOrder(1);
+                                }
+                            }} key={id}>
+                                <div className=' bg-black/0 relative'>
+                                    <p>{head}</p>
+                                    <div className=' bg-white/0  absolute right-0 top-0 bottom-0 flex items-center justify-center'>
+                                    <svg  style={{
+                                            transform: `rotate(${order === 0 ? 90 : order === 1 ? 180 : 0}deg)`,
+                                            transition: 'transform 0.3s ease',
+                                        }}
+                                     xmlns="http://www.w3.org/2000/svg" fill='white' x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
+                                        <path d="M 7.5 7 A 1.50015 1.50015 0 1 0 7.5 10 L 26.5 10 A 1.50015 1.50015 0 1 0 26.5 7 L 7.5 7 z M 33.478516 13.5 A 1.50015 1.50015 0 0 0 32.439453 13.939453 L 25.439453 20.939453 A 1.50015 1.50015 0 1 0 27.560547 23.060547 L 32 18.621094 L 32 40.5 A 1.50015 1.50015 0 1 0 35 40.5 L 35 18.621094 L 39.439453 23.060547 A 1.50015 1.50015 0 1 0 41.560547 20.939453 L 34.560547 13.939453 A 1.50015 1.50015 0 0 0 33.478516 13.5 z M 7.5 15 A 1.50015 1.50015 0 1 0 7.5 18 L 22.5 18 A 1.50015 1.50015 0 1 0 22.5 15 L 7.5 15 z M 7.5 23 A 1.50015 1.50015 0 1 0 7.5 26 L 18.5 26 A 1.50015 1.50015 0 1 0 18.5 23 L 7.5 23 z M 7.5 31 A 1.50015 1.50015 0 1 0 7.5 34 L 14.5 34 A 1.50015 1.50015 0 1 0 14.5 31 L 7.5 31 z M 7.5 39 A 1.50015 1.50015 0 1 0 7.5 42 L 10.5 42 A 1.50015 1.50015 0 1 0 10.5 39 L 7.5 39 z"></path>
+                                    </svg>
+
+                                    </div>
+                                </div>
+                            </th>
+                            :
                             <th key={id}>{head}</th>
                         ))}
+                        {/* {headings.map((head, id) => (
+                            <th key={id}>{head}</th>
+                        ))} */}
                     </tr>
                 </thead>
                 <tbody>
                     {rows.map((row) => (
                         <Row
-                            sno={rows.indexOf(row)+1}
+                            sno={startSno + rows.indexOf(row)+1}
                             name={row.name}
                             companyName={row.companyName}
                             rollNo={row.rollNo}
@@ -36,6 +70,7 @@ const Table: React.FC<TableProps> = ({ headings, rows }) => {
                     ))}
                 </tbody>
             </table>
+
         </div>
     );
 };
